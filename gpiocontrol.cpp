ecb3580ab8f0d6c43b4f9fba2c78aed6dd4f8c72
cean1024 <cean1024@163.com>
2018-04-23 09:31:21 +0800

    rapsberry gpio contrl class

--- gpiocontrol.cpp
+++ gpiocontrol.cpp
@@ -2,5 +2,11 @@
 
 GpioControl::GpioControl()
 {
+    wiringPiSetup();
+    for(int i=DEFAULTPINSTART;i<=DEFAULTPINEND;i++){
+
+        pinMode(i, OUTPUT);
+    }
+
 
 }
--- gpiocontrol.h
+++ gpiocontrol.h
@@ -1,11 +1,38 @@
 #ifndef GPIOCONTROL_H
 #define GPIOCONTROL_H
+#include <iostream>
+#include "wiringPi.h"
 
+#define DEFAULTPINSTART 2
+#define DEFAULTPINEND 6
 
+using namespace std;
 class GpioControl
 {
 public:
     GpioControl();
+    GpioControl(int pin,int mode)
+    {
+        wiringPiSetup();
+        this->pin = pin;
+        pinMode(pin, mode);
+    }
+    void setallhigh(){
+        cout <<__func__ <<endl;
+        for(int i=DEFAULTPINSTART;i<=DEFAULTPINEND;i++)
+           digitalWrite(i, HIGH);
+    }
+    void setalllow(){
+        cout <<__func__ <<endl;
+        for(int i=DEFAULTPINSTART;i<=DEFAULTPINEND;i++)
+           digitalWrite(i, LOW);
+    }
+    int getvalue(int pin) {
+        return digitalRead(pin);
+    }
+
+private:
+    int pin;
 };
 
 #endif // GPIOCONTROL_H
