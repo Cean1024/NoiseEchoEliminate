f34ef6d56b29321d190c8c8fa7c253d404438527
cean1024 <cean1024@163.com>
2018-05-03 16:29:11 +0800

    添加关键字检测类

--- keyword/keyworddetect.h
+++ keyword/keyworddetect.h
@@ -1,11 +1,45 @@
 #ifndef KEYWORDDETECT_H
 #define KEYWORDDETECT_H
 
+#include "common.h"
+#include <iostream>
+#include <fstream>
+#include <string.h>
 
-class KeyWordDetect
+#define READLEN 200
+
+using namespace std;
+class keyword{
+public:
+    r_status readmel_coe(string filepath){
+
+        ifstream fp(filepath,ios::in|ios::binary);
+        char buf[READLEN];
+
+        while(!fp.eof()) {
+            memset(buf,0,READLEN);
+            fp.getline(buf,READLEN);
+            cout << buf <<endl ;
+        }
+        cout << endl;
+        fp.close();
+        return SUCCESS;
+    }
+
+
+
+
+};
+
+class KeyWordDetect:public keyword
 {
 public:
     KeyWordDetect();
+    KeyWordDetect(string filepath)
+    {
+        readmel_coe(filepath);
+
+    }
 };
 
 #endif // KEYWORDDETECT_H
