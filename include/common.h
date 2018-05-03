37f000dc045caad1e7605d634dd48613370fb3dc
cean1024 <cean1024@163.com>
2018-05-03 16:27:48 +0800

    调整返回值参数到common中

--- include/common.h
+++ include/common.h
@@ -4,8 +4,40 @@
 /*audio configration*/
 #define CHANNLE 2
 #define BITS 16
-#define SAMPLERATE 48000
-#define FRAMESIZE (SAMPLERATE/50)
+#define SAMPLERATE 16000
+#define FRAMESIZE (SAMPLERATE/100)
 #define AFRAMEBUFSIZE (FRAMESIZE * CHANNLE *BITS /8)
+
+
+
+
+
+/*common config*/
+#define r_status int
+
+enum returnstatus {
+
+    ERROR = -2,
+    FAILED = -1,
+    SUCCESS=0,
+    PSUCCESS,
+    PFAILED,
+    FSUCCES,
+    FFAILD,
+    ISUCCESS,
+    IFAILED,
+    IGENOR,
+    NEEDUPDATA,
+    NOTNEEDUPDATA,
+    EXIST,
+    NOTEXIST,
+    WAITING,
+    YES,
+    NO,
+    PLAYSTART,
+    PLAYSTOP,
+    PLAYRUNING,
+    PLAYWAITING
+};
 
 #endif
