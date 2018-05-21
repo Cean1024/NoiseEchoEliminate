689e254dc0df6726c4b9ac1076d5f0fb887c514b
cean1024 <cean1024@163.com>
2018-05-21 14:25:07 +0800

    add pocketsphinx interface

---
+++ keyword/pocketsphinxkeyword.cpp
@@ -0,0 +1,6 @@
+#include "pocketsphinxkeyword.h"
+
+PocketSphinxKeyword::PocketSphinxKeyword()
+{
+
+}
---
+++ keyword/pocketsphinxkeyword.h
@@ -0,0 +1,11 @@
+#ifndef POCKETSPHINXKEYWORD_H
+#define POCKETSPHINXKEYWORD_H
+
+
+class PocketSphinxKeyword
+{
+public:
+    PocketSphinxKeyword();
+};
+
+#endif // POCKETSPHINXKEYWORD_H
