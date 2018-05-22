15cc5a63bf8b2bd6c465e1914128c0b78f7e46fb
cean1024 <cean1024@163.com>
2018-05-22 16:56:05 +0800

    add tcp client class

--- network/netclient.cpp
+++ network/netclient.cpp
@@ -2,5 +2,54 @@
 
 netclient::netclient()
 {
+    init_client(SERVERIP,PortNumber);
+}
+netclient::netclient(std::string serverip, int port)
+{
 
+    init_client(serverip,port);
+
+}
+
+netclient::~netclient()
+{
+
+}
+
+
+int netclient::init_client(std::string serverip,int port)
+{
+    //struct sockaddr_in serverid;
+
+    if  (( clientfd = socket ( AF_INET ,  SOCK_STREAM , 0 ) ) < 0 ) {
+        perror("LISTEN");
+        return FAILED;
+    }
+    memset(&server_addr, 0 ,sizeof (struct sockaddr_in));
+    server_addr.sin_family = AF_INET;
+    //strncpy(IP_ADDR,serverip.c_str(),serverip.size());
+    //PORT = port;
+    server_addr.sin_addr.s_addr = inet_addr( serverip.c_str() );
+    server_addr.sin_port = htons( port );
+    if ( ( connect ( clientfd ,(struct sockaddr *)&server_addr ,
+                     sizeof (struct sockaddr_in) ) ) < 0) {
+        LogOut("connect error");
+        perror("connect ");
+        close(clientfd);
+        clientfd = -1;
+        return FAILED;
+    }
+    return SUCCESS;
+
+}
+
+void netclient::s_close()
+{
+    if(clientfd>0)close(clientfd);
+    clientfd =-1;
+}
+int netclient::senddata(char *buf,int size)
+{
+    if(clientfd<0) return ERROR;
+    return send(clientfd , buf , size , 0);
 }
--- network/netclient.h
+++ network/netclient.h
@@ -1,11 +1,38 @@
 #ifndef NETCLIENT_H
 #define NETCLIENT_H
+#include "common.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstring>
 
+#define PortNumber 9009
+
+#define SERVERIP "192.168.199.245"
 
 class netclient
 {
 public:
     netclient();
-};
+    netclient(std::string serverip, int port);
+    ~netclient();
 
+
+    int senddata(char *buf,int size);
+    void s_close();
+
+protected:
+    int init_client(std::string serverip, int port);
+private:
+    int ret;
+    int sockfd;
+
+    int clientfd;
+
+    struct sockaddr_in server_addr;
+    struct hostent * host;
+};
 #endif // NETCLIENT_H
