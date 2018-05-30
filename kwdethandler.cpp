a7c81cb81d2fde4aa8f6c53c08e737214ab06060
cean1024 <cean1024@163.com>
2018-05-30 19:23:30 +0800

    添加关键词线程处理类

---
+++ kwdethandler.cpp
@@ -0,0 +1,6 @@
+#include "kwdethandler.h"
+
+KWDetHandler::KWDetHandler()
+{
+
+}
---
+++ kwdethandler.h
@@ -0,0 +1,11 @@
+#ifndef KWDETHANDLER_H
+#define KWDETHANDLER_H
+
+
+class KWDetHandler
+{
+public:
+    KWDetHandler();
+};
+
+#endif // KWDETHANDLER_H
